6b8251b3d56cb0ba034ee4ae728c7e6761d0e2bb
Stephen Chryn <SRCthird@gmail.com>
2024-04-01 12:29:55 -0400

    Organize core items

--- src/core/service.h
+++ src/core/service.h
@@ -1,16 +1,29 @@
 #ifndef SERVICE_H
 #define SERVICE_H
+
 #include <crow.h>
 #include <crow/app.h>
 #include <crow/http_response.h>
 #include <crow/mustache.h>
 #include <crow/utility.h>
+#include "utils.h"
 
 class service {
 public:
-  crow::mustache::rendered_template home();
-  void staticDir(const crow::request &req, crow::response &res, std::string fileName);
-  void apiCall(const crow::request &req, crow::response &res);
+  void staticDir(const crow::request &req, crow::response &res,std::string fileName) {
+    std::ifstream in("static/" + fileName, std::ifstream::in | std::ifstream::binary);
+    if (in) {
+      std::ostringstream contents;
+      contents << in.rdbuf();
+      in.close();
+      res.body = contents.str();
+      res.add_header("Content-Type", getContentType(fileName));
+    } else {
+      res.code = 404;
+      res.body = "File not found";
+    }
+    res.end();
+  }
 private:
 };
 
--- src/driver.cpp
+++ src/driver.cpp
@@ -1,5 +1,6 @@
-#include "controller.h"
-#include "../user/user.controller.h"
+#include "core/controller.h"
+#include "api/user/user.controller.h"
+#include "app/app.controller.h"
 
 int main() {
   crow::SimpleApp app;
@@ -8,6 +9,7 @@ int main() {
   // Import Controllers
   controller ctrl(app, env);
   user_controller user_ctrl(app, env);
+  app_controller app_ctrl(app, env);
   // End Import Controllers
   
   ctrl.run();
