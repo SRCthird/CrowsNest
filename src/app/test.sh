b7881d4df7e787ca5d446a8be9566834dd6dcdbc
Stephen Chryn <SRCthird@gmail.com>
2024-04-01 11:27:14 -0400

    Update routes

--- src/app/test.sh
+++ src/app/test.sh
@@ -3,48 +3,43 @@
 # Base URL of your Crow application
 BASE_URL="http://localhost:18080"
 
-# Test the home page
-echo "Testing home page:"
-curl "$BASE_URL/"
-echo -e "\n"
-
 # Test creating a single user
 echo "Testing creating a single user:"
-curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "name": "John Doe"}' "$BASE_URL/create"
+curl -X POST -H "Content-Type: application/json" -d '{"id": 1, "name": "John Doe"}' "$BASE_URL"
 echo -e "\n"
 
 # Test creating multiple users
 echo "Testing creating multiple users:"
-curl -X POST -H "Content-Type: application/json" -d '[{"id": 2, "name": "Jane Doe"}, {"id": 3, "name": "Jim Doe"}]' "$BASE_URL/create_bulk"
+curl -X POST -H "Content-Type: application/json" -d '[{"id": 2, "name": "Jane Doe"}, {"id": 3, "name": "Jim Doe"}]' "$BASE_URL/bulk"
 echo -e "\n"
 
 # Test getting a single user
 echo "Testing getting a single user:"
-curl "$BASE_URL/get/1"
+curl "$BASE_URL/1"
 echo -e "\n"
 
 # Test getting all users
 echo "Testing getting all users:"
-curl "$BASE_URL/get"
+curl "$BASE_URL"
 echo -e "\n"
 
 # Test getting users by ids
 echo "Testing getting users by ids:"
-curl "$BASE_URL/get?id=2,3"
+curl "$BASE_URL/?id=2,3"
 echo -e "\n"
 
 # Test updating a user
 echo "Testing updating a user:"
-curl -X PUT -H "Content-Type: application/json" -d '{"id": 1, "name": "John Updated"}' "$BASE_URL/update"
+curl -X PUT -H "Content-Type: application/json" -d '{"id": 1, "name": "John Updated"}' "$BASE_URL"
 echo -e "\n"
 
 # Test deleting a single user
 echo "Testing deleting a single user:"
-curl -X DELETE "$BASE_URL/delete/1"
+curl -X DELETE "$BASE_URL/1"
 echo -e "\n"
 
 # Test deleting multiple users
 echo "Testing deleting multiple users:"
-curl -X DELETE "$BASE_URL/delete?id=2,3"
+curl -X DELETE "$BASE_URL/?id=2,3"
 echo -e "\n"
 
