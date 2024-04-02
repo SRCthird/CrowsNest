d14b6f9a51a34a9af0d3d9157853c290854c530f
Stephen Chryn <SRCthird@gmail.com>
2024-04-02 16:08:27 -0400

    Create home service to display users

--- src/app/app.service.h
+++ src/app/app.service.h
@@ -6,17 +6,31 @@
 #include <crow/http_response.h>
 #include <crow/mustache.h>
 #include <crow/utility.h>
+#include <vector>
+#include "../api/user/user.service.h"
 
 class app_service {
 public:
 
   app_service() {}
+  user_service userSvc;
 
   crow::mustache::rendered_template home() {
     auto page = crow::mustache::load("base.html");
+    auto child = crow::mustache::load("usersList.html");
+
     crow::mustache::context ctx;
     ctx["PUBLIC_URL"] = "/static";
-    ctx["content"] = crow::mustache::load("child.html").render_string();
+    std::vector<user_service::User> users = userSvc.read();
+
+    for (auto& user : users) {
+      ctx["users"][user.id] = crow::mustache::context{
+        {"id", user.id},
+        {"name", user.name}
+      };
+    }
+    ctx["content"] = child.render_string(ctx);
+
     return page.render(ctx);
   }
 
